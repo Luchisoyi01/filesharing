3cd9e69fe8371f5d068e791787a51cddb48886b3
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-16 23:13:27 +0300

    Removed unnessary spaces

--- snake.py
+++ snake.py
@@ -99,10 +99,9 @@ def gameLoop():
         snake_List.append(snake_Head)
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
-
         for x in snake_List[:-1]:
             if x == snake_Head:
-                game_close = True
+              game_close = True
 
         our_snake(snake_block, snake_List)
 
